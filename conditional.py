76ed22874b109b981035b3255dd7f3bd39a2ef74
Pema406 <03230195.gcbs@rub.edu.bt>
2024-03-11 21:49:25 +0600

    conditional

---
+++ conditional.py
@@ -0,0 +1,18 @@
+print('*')
+print('+')
+print('-')
+print('/')
+whatusertyped = input()
+print('usertyped:', whatusertyped)
+print('user input-type',type(whatusertyped))
+print('-----------------')
+if whatusertyped =="+":
+    print('Doing Addition')
+print ('doing more Addition')
+if whatusertyped == "-":
+    print('doing subtraction')
+    print('doing more subtraction')
+if whatusertyped == "*":
+    print('doing multiplication')
+    print('doing ,more multiplication')
+
