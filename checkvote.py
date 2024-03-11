14588649d124ec176f344fa200fc4c1735ad2390
Pema406 <03230195.gcbs@rub.edu.bt>
2024-03-11 22:15:12 +0600

    checkvote

---
+++ checkvote.py
@@ -0,0 +1,10 @@
+userinput = input('please type your age:')
+userage = int(userinput)
+if userage >=18:
+    print('you can vote')
+else:
+    print('you cannot vote')
+if userage <18:
+    print('you cannot vote')
+elif userage<18:
+    print('you cannot vote17')
