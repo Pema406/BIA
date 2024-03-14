bd50da202def412427197616440ad4afd02fe1a7
Pema406 <03230195.gcbs@rub.edu.bt>
2024-03-14 14:18:42 +0600

    datatype-sample

---
+++ datatypes.py
@@ -0,0 +1,29 @@
+#creation of array
+array1 = [1,2,3, "thimphu", 2.5]
+
+#print(array1)
+#retriving
+element1 = array1[0]
+element2 = array1[4]
+#print(element1)
+#print(element2)
+lastElements= array1[-1]
+#print(lastElements)
+#modifying element
+array1[0]= 10
+#print(array1)
+#getting number of element in the array
+no_of_elements=len(array1)
+#print(no_of_elements)
+#slicing
+elements = array1[0:2]
+#print(elements)
+arr1= [1,10]
+arr2= ['thimphu','wangdi']
+arr3= arr2 + arr1
+number_to_check= 2
+result= 
+#print(arr3)
+arrvariable = [1,2,3]
+arrvariable.append(4)
+
